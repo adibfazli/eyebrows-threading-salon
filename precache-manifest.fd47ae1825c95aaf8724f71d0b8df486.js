a8bf3ec895e993cb9933b662b18de005b1c772e9
Adib Fazli <adib.fazlii@gmail.com>
2020-07-24 11:09:27 -0700

    Updates

---
+++ precache-manifest.fd47ae1825c95aaf8724f71d0b8df486.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95e21cdcd93468a48d736fd86384dccd",
+    "url": "/eyebrows-threading-salon/index.html"
+  },
+  {
+    "revision": "01c70e1a4635e7708733",
+    "url": "/eyebrows-threading-salon/static/css/main.c6ac11ec.chunk.css"
+  },
+  {
+    "revision": "4a13c7a032f51dc2ac6b",
+    "url": "/eyebrows-threading-salon/static/js/2.0b9d9067.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/eyebrows-threading-salon/static/js/2.0b9d9067.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01c70e1a4635e7708733",
+    "url": "/eyebrows-threading-salon/static/js/main.a9199e47.chunk.js"
+  },
+  {
+    "revision": "1c80bd2509629b7e065f",
+    "url": "/eyebrows-threading-salon/static/js/runtime-main.c4a7946c.js"
+  },
+  {
+    "revision": "7a4b1a5739cc4d6bbb9c72411c500fcb",
+    "url": "/eyebrows-threading-salon/static/media/1.7a4b1a57.png"
+  },
+  {
+    "revision": "56034f9f9bb95cbf53e909b7ec45eb48",
+    "url": "/eyebrows-threading-salon/static/media/2.56034f9f.png"
+  },
+  {
+    "revision": "6347f181b4c93e7acc0a5ad1fd5b7f3e",
+    "url": "/eyebrows-threading-salon/static/media/3.6347f181.png"
+  },
+  {
+    "revision": "b5386e5067bdc6d507c30f265958c0dc",
+    "url": "/eyebrows-threading-salon/static/media/4.b5386e50.png"
+  },
+  {
+    "revision": "2e6aba6007ca4af8d192726b38373975",
+    "url": "/eyebrows-threading-salon/static/media/5.2e6aba60.png"
+  },
+  {
+    "revision": "8cb9c4ac558c363a575797aef8eb15f1",
+    "url": "/eyebrows-threading-salon/static/media/6.8cb9c4ac.png"
+  },
+  {
+    "revision": "8aadff94a1e3305dc35001888d90fd3e",
+    "url": "/eyebrows-threading-salon/static/media/agencyBorn.8aadff94.jpg"
+  },
+  {
+    "revision": "aed986a7b203833e8ee874c4243c5850",
+    "url": "/eyebrows-threading-salon/static/media/airplaine.aed986a7.jpg"
+  },
+  {
+    "revision": "bf6099aff0e1c9021f16fdf7aa4e1e63",
+    "url": "/eyebrows-threading-salon/static/media/expansion.bf6099af.jpg"
+  },
+  {
+    "revision": "40ef2c697f204f041fda525dc20eb218",
+    "url": "/eyebrows-threading-salon/static/media/header-bg.40ef2c69.jpg"
+  },
+  {
+    "revision": "f40ed5b1e791e9d2479ba8b5e67f2df8",
+    "url": "/eyebrows-threading-salon/static/media/map-image.f40ed5b1.png"
+  },
+  {
+    "revision": "907d66d8bb58d1a0b6b3bdba82c77a5d",
+    "url": "/eyebrows-threading-salon/static/media/profileIcon.907d66d8.jpg"
+  },
+  {
+    "revision": "fee76645246b78d9e9ac9e44172a6db1",
+    "url": "/eyebrows-threading-salon/static/media/sudi.fee76645.jpg"
+  },
+  {
+    "revision": "cb7ae9c30a90c62a58dc526c4ba6d4bb",
+    "url": "/eyebrows-threading-salon/static/media/transition.cb7ae9c3.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.098e71636b08efcc2ef7e9a315ab977c.js"
+  "/eyebrows-threading-salon/precache-manifest.fd47ae1825c95aaf8724f71d0b8df486.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/eyebrows-threading-salon/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
